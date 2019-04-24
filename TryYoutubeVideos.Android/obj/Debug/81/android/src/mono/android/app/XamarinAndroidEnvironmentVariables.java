fdd865c4d7ac07018f5d42af0271ce2c37a801a1
Erick Almeida <Erick.Almeida@transmax.com.au>
2019-04-24 17:30:40 +1000

    Adding Video Interface Here

---
+++ TryYoutubeVideos.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "9c5610e4-2394-4dab-a39b-32ef5f1d7cdf",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
